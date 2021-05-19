06b660988aebd6e4ba324fa1b0713b73046768b8
James Cassidy <james.cassidy03@gmail.com>
2021-05-19 12:23:05 +0100

    More Javascript functionailty

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-const navToggle = document.querySelector('nav-toogle');
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link'); //All the nav links
+
 
 navToggle.addEventListener('click', () => {
-document.body.classList.toggle('nav-open');
+    document.body.classList.toggle('nav-open'); //When clicked, the navigation will open
 });
+
+navLinks.forEach(link => {              //when a link is clicked
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open'); //Remove menu when clicking on that specfic area
+    })
+})
